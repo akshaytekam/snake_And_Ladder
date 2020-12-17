e846de6394b7ec774ece877943d35dc6fcf2fba4
Akshay Tekam <akshaytekam@atlassian.com>
2020-12-17 11:10:52 +0530

    UC2 commit

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,15 +8,16 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
          int N=100;    //Number of cells
           
-         int number_Of_player=1;
-         int player_position=0;
+         int NUMBER_OF_PLAYER=1;
+         int PLAYER_POSITION=0;
          System.out.println("Play Snake and Ladder");
          //UC1
+        
       Random rand = new Random();   //object of random class
-      int rand_int1 = rand.nextInt(6);  //Print value from 0 to 5
-      System.out.println("Random Numbers: "+rand_int1);
-         
-          
+      int RAND_INT1 = rand.nextInt(6);  //Print value from 0 to 5
+      System.out.println("Roll Dice");
+      System.out.println("Random Numbers: "+RAND_INT1);
+    
     //UC2
          }
     }
