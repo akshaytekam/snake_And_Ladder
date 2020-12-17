d0768a00c823c18ac68018cb88ceb177a74d81c2
Akshay Tekam <akshaytekam@atlassian.com>
2020-12-17 10:33:28 +0530

    First commit

---
+++ SnakeAndLadder.java
@@ -0,0 +1,27 @@
+ 
+package snakeand.ladder;
+ 
+public class SnakeAndLadder {
+    
+    int cell;
+    int dist;
+ 
+    public static void main(String[] args) {
+         int N=30;
+         int[] sheet=new int[N];
+         
+         for (int i=0; i<N; i++){
+             sheet[i]=-1;
+             
+             // Ladders
+		sheet[2] = 21;
+		sheet[4] = 7;
+		sheet[10] = 25;
+		sheet[19] = 28;
+
+		 
+
+         }
+    }
+    
+}
